64a3404d5450a090035c098035108766702ebc16
samuelmico9 <mbongo.samuel@yahoo.fr>
2022-12-11 05:51:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - main block
  * Return: o
  */
-
 int main(void)
-
 {
         puts("\"Programming is like building a multilingual puzzle");
 	return (0);
