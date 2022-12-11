5f84e94dcbbc9403b59cc22ad4b8bd67c91cf376
samuelmico9 <mbongo.samuel@yahoo.fr>
2022-12-11 02:41:12 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - main block
- * Return: 0
+ * Return: o
  */
- int main(void)
+int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
 	return (0);
